de1da5db4b2217b657d758a7e8d104cd8c0201d4
cdx <cdx@192.168.1.102>
2017-12-28 07:58:28 +0800

    Organize configuration files.

--- wishingwell/src/main/java/org/cdx/ww/dao/UserMapper.java
+++ wishingwell/src/main/java/org/cdx/ww/dao/UserMapper.java
@@ -1,7 +1,9 @@
 package org.cdx.ww.dao;
 
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserMapper {
 	@Select("SELECT count(*) FROM users")
 	int getUserCount();
